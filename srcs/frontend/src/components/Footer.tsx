69f151a3848f14cf2d35b8699374f89b3f447397
jumichel <jumichel@student.42.fr>
2026-03-07 11:14:52 +0100

    changed general background + fixed banner responsive problem for menu (search bar buttons remaining)

--- srcs/frontend/src/components/Footer.tsx
+++ srcs/frontend/src/components/Footer.tsx
@@ -1,35 +1,42 @@
 import { Footer, Content } from '@allxsmith/bestax-bulma';
 import { NavLink } from 'react-router';
+import "./ui/parchment.css";
 
 const MyFooter = () => {
 	return (
 		<Footer className="my-footer">
-			<Content textAlign="centered">
-				<p>
-				<strong>DungeonNoDragon</strong> a transcendence project by{' '}
-				<strong><a href="">agruet</a></strong>{', '}
-				<strong><a href="">jumichel</a></strong>{', '}
-				<strong><a href="">mprokosc</a></strong>{', '}
-				<strong><a href="">ndabbous</a></strong>{', '}
-				<strong><a href="">tpinton</a></strong>.
-				</p>
-			</Content>
-			<Content textAlign="centered">
-				<p>
-					Find out about our{' '}
-					<NavLink to="/privacy_policy" aria-label='Privacy Policy link'>
-						<strong>Privacy Policy</strong>
-					</NavLink>{' and '}
-					<NavLink to="/terms_of_service" aria-label='Terms of Service link'>
-						<strong>Terms of Service</strong>
-					 </NavLink>.
-				</p>
-			</Content>
-			<Content textAlign="centered">
-				<p>
-					@2026 Piscine of July 2024 Team
-				</p>
-			</Content>
+			<img src="../assets/parchment-left.svg" />
+			<div id="parchment-center">
+				<div className='parchment-text'>
+					<Content textAlign="centered" style={{marginTop: 'auto'}}>
+						<p>
+						<strong>DungeonNoDragon</strong> a transcendence project by{' '}
+						<strong><a href="">agruet</a></strong>{', '}
+						<strong><a href="">jumichel</a></strong>{', '}
+						<strong><a href="">mprokosc</a></strong>{', '}
+						<strong><a href="">ndabbous</a></strong>{', '}
+						<strong><a href="">tpinton</a></strong>.
+						</p>
+					</Content>
+					<Content textAlign="centered">
+						<p>
+							Find out about our{' '}
+							<NavLink to="/privacy_policy" aria-label='Privacy Policy link'>
+								<strong>Privacy Policy</strong>
+							</NavLink>{' and '}
+							<NavLink to="/terms_of_service" aria-label='Terms of Service link'>
+								<strong>Terms of Service</strong>
+								</NavLink>.
+						</p>
+					</Content>
+					<Content textAlign="centered" style={{marginBottom: 'auto'}}>
+						<p>
+							@2026 Piscine of July 2024 Team
+						</p>
+					</Content>
+				</div>
+			</div>
+			<img src="../assets/parchment-right.svg" />
 		</Footer>
 	)
 }
